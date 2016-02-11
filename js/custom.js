c42cf66136a6fbeb04058d2c70b546f40fc7ae24
Divay Prakash <divayprakash3@gmail.com>
2016-02-11 12:31:14 +0530

    console logging stopped, fixes #7

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,19 @@
 function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
     var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log("Name: " + profile.getName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
+    //console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    //console.log("Name: " + profile.getName());
+    //console.log("Image URL: " + profile.getImageUrl());
+    //console.log("Email: " + profile.getEmail());
     document.getElementById('signout').style.display = 'inline';
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
+    //console.log("ID Token: " + id_token);
 }
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+        //console.log('User signed out.');
     });
     document.getElementById('signout').style.display = 'none';
 }
